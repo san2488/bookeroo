ac5b9f70f351f6afd20150c6d090027e0dae301a
Jinay Mehta <mjinay@gmail.com>
2013-12-01 19:00:51 -0500

    K-NN with New Books taken into account

--- knn.py
+++ knn.py
@@ -15,15 +15,22 @@ ratings_train, ratings_test = get_sample_data(ratings, 0.9)
 b = get_data_non0(ratings_train)
 r = get_data_non0(ratings_train)
 t = get_data_non0(ratings_test)
+tb = get_data_non0(ratings_test)
 
+tb_mu = np.average(tb['Book-Rating'])
+#print tb_mu
 
 r.set_index(['User-ID','ISBN'],inplace = True)
 t.set_index(['User-ID','ISBN'],inplace = True)
 b.set_index(['ISBN','User-ID'],inplace = True)
+tb.set_index(['ISBN','User-ID'],inplace = True)
 
 ug = r.groupby(level = ['User-ID','ISBN'])['Book-Rating']
 tg = t.groupby(level = ['User-ID','ISBN'])['Book-Rating']
 bg = b.groupby(level = ['ISBN','User-ID'])['Book-Rating']
+tbg = tb.groupby(level = ['ISBN','User-ID'])['Book-Rating']
+
+delta = 20
 
 mat = {}
 
@@ -56,11 +63,15 @@ for k1,k2 in bg:
 	mat_book[k1[0]] = tmp
 	k1[0], k1[1], k2[0]
 
-'''
-knn = []
-rmse = 0
-count = 0	
-'''
+
+mat_book_test = {}
+for k1,k2 in tbg:
+	tmp = {}
+	if(mat_book_test.has_key(k1[0])):
+		tmp = mat_book_test[k1[0]]
+	tmp[k1[1]] = k2[0]
+	mat_book_test[k1[0]] = tmp
+	k1[0], k1[1], k2[0]
 
 
 for i in range(1,11):
@@ -69,19 +80,18 @@ for i in range(1,11):
 	count = 0	
 	for k1,v1 in mat_test.iteritems():
 		for k2,v2 in v1.iteritems():
+			predict_val = 0
 			if(k2 in mat_book):
-				#t = {}
-				#t['Book-Rating'] = v2
-				#u = get_nn(k1,v1,k2,mat,mat_book[k2].keys())
-				#t['pred-Book-Rating'] = get_nn(k1,v1,k2,mat,mat_book[k2].keys(),i)
-				#mat[u][k2]	
-				#t['rmse'] = (((t['Book-Rating'] - t['pred-Book-Rating'])**2)**0.5)
-				#rmse += t['rmse']
 				predict_val = get_nn(k1,v1,k2,mat,mat_book[k2].keys(),i)				
-				rmse += (((v2 - predict_val)**2)**0.5)
-				count += 1
-				#knn.append(t)
-	rmse = rmse/count
-	print "k= ",i," RMSE: ",rmse
+			else:
+				predict_val = baseline_dist(v1.values(),mat_book_test[k2].values(),tb_mu,delta)
+				#print v2, " ",predict_val
+			
+			rmse += ((v2 - predict_val)**2)
+			count += 1
+
+	rmse = (rmse/count)**0.5
+
+	print "k = ",i," RMSE: ",rmse
 
 #1.46629419068
--- utils.py
+++ utils.py
@@ -7,19 +7,21 @@ def get_rmse_from_data(data, actual_col="Book-Rating", predicted_col="pred-Book-
 def get_rmse(d1, d2):
     return np.sqrt(((d1 - d2)**2).mean())    
 
-def get_distance(v1,v2,mu):
+def get_distance(v1,v2,exst_book,mu):
 	sum = 0
 	flag = False
 
 	for k,v in v1.iteritems():
-		if(k in v2):
-			sum += ((v - v2[k])**2)
-			flag = True
-		else:
-			sum += ((v - mu)**2)
+		#print k, exst_book
+		if(k != exst_book):
+			if(k in v2):
+				sum += ((v - v2[k])**2)
+				flag = True
+			else:
+				sum += ((v - mu)**2)
 
 	for k, v in v2.iteritems():
-		if(k not in v1):
+		if(k not in v1 and k != exst_book):
 			sum += ((v - mu)**2)
 
 	sum = np.sqrt(sum)
@@ -29,12 +31,31 @@ def get_distance(v1,v2,mu):
 		return 1000
 
 
+def baseline_dist(v1,v2,mu,delta):
+
+	bias_u = 0
+	bias_b = 0
+
+	for v in v1:
+		bias_u += (v - mu)
+
+	bias_u /= float(delta + len(v1))
+	
+	for v in v2:
+		bias_b += (v - mu)
+	
+	bias_b /= float(delta + len(v2))
+	m = mu + bias_u + bias_b
+	#print v1, len(v1), v2, len(v2), m
+	return m
+
+
 def get_nn(u1,v1,k2,mat,users,knn):
 	
 	d = {}
 	for u in users:
 		if(u in mat):
-			d[u] = get_distance(v1,mat[u],0)
+			d[u] = get_distance(v1,mat[u],k2,0)
 	
 
 	
